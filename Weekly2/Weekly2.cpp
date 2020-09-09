979805291269ee62be16e9e232cb3d45ef84b0e3
Eddy <Edvardh@hotmail.no>
2020-09-09 16:45:55 +0200

    Add project files.

---
+++ Weekly2/Weekly2.cpp
@@ -0,0 +1,173 @@
+//Weekly2 assignment by Edvard Helgetun
+
+#include <iostream>
+#include <conio.h>
+
+int input{0};
+
+void task6() 
+{
+    char grade = ' ';
+    std::cout << "\nWhat grade would you like to have in Programming 1?   -";
+    grade = _getch();
+    std::cout << grade;
+
+    switch (grade) {
+    case 'A':
+        std::cout << "\nOutstanding!";
+        break;
+    case 'a':
+        std::cout << "\nOutstanding!";
+        break;
+    case 'B':
+        std::cout << "\nVery Good";
+        break;
+    case 'b':
+        std::cout << "\nVery Good";
+        break;
+    case 'C':
+        std::cout << "\nGood";
+        break;
+    case 'c':
+        std::cout << "\nGood";
+        break;
+    case 'D':
+        std::cout << "\nSome flaws";
+        break;
+    case 'd':
+        std::cout << "\nSome flaws";
+        break;
+    case 'E':
+        std::cout << "\nNot very good...";
+        break;
+    case 'e':
+        std::cout << "\nNot very good...";
+        break;
+    case 'F':
+        std::cout << "\nFail";
+        break;
+    case 'f':
+        std::cout << "\nFail";
+        break;
+    default:
+        std::cout << "\nThats not a grade!";
+        break;
+    }
+
+    std::cout << "\n\n";
+    system("pause");
+}
+
+void task5() 
+{
+    int i{ 5 };
+
+    do
+    {
+        std::cout << "i = " << i << std::endl;
+        i += 5;
+    } while (i <51);
+
+    std::cout << "Printed from 5 to 50 with a DO WHILE loop: \n";
+    system("pause");
+}
+
+void task4() 
+{
+    for (int i = 5; i < 51; i += 5) 
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+    std::cout << "Printed from 5 to 50 with a FOR loop: \n";
+    system("pause");
+}
+
+void task3() 
+{
+    int i{ 5 };
+
+    while (i < 51) 
+    {
+        std::cout << "i = " << i << std::endl;
+        i += 5;
+    }
+    std::cout << "Printed from 5 to 50 with a WHILE loop: \n";
+    system("pause");
+}
+
+void task2() 
+{
+    for (int i = 100; i > 0; i--)
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+    std::cout << "Printed from 100 to 1 with a FOR loop: \n";
+
+    system("pause");
+}
+
+void task1()
+{
+    int i{ 1 };
+
+    while (i < 101)
+    {
+        std::cout << "i = " << i << std::endl;
+        i++;
+    }
+    std::cout << "Printed from 1 to 100 with a WHILE loop: \n";
+
+    system("pause");
+    
+    for (int j = 1; j < 101; j++) 
+    {
+        std::cout << "j = " << j << std::endl;
+    }
+    std::cout << "Printed from 1 to 100 with a FOR loop: \n";
+
+    system("pause");
+}
+
+int getInput()
+{
+    int p{ 0 };
+    p = _getch() - '0';
+
+    return p;
+}
+
+int main()
+{
+    while (true) {
+        std::cout << "Choose a task between 1 and 6:" << std::endl;
+        input = getInput();
+        std::cout << "you typed " << input << std::endl;
+
+        switch (input) {
+        case 1:
+            task1();
+            break;
+        case 2:
+            task2();
+            break;
+        case 3:
+            task3();
+            break;
+        case 4:
+            task4();
+            break;
+        case 5:
+            task5();
+            break;
+        case 6:
+            task6();
+            break;
+        default:
+            std::cout << "Invalid choice!\n";
+            system("pause");
+            break;
+        }
+
+        system("cls");
+    }
+}
